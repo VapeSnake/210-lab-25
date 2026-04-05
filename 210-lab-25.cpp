450c406baf8e6cc1d2aa3e1f8d61e07b651bae3d
Noel Mier-Luna <noel.lmier@gmail.com>
2026-04-05 16:26:12 -0700

    Final commit

--- 210-lab-25.cpp
+++ 210-lab-25.cpp
@@ -8,19 +8,18 @@
 #include <iomanip>
 using namespace std;
 using namespace std::chrono;
-
 // Functions to help clean space in main.
-int vecRead(vector <string> &v);
-int listRead(list <string> &l);
-int setRead(set <string> &s);
-int vecSort(vector <string> &v);
-int listSort(list <string> &l);
-int vecInsert(vector <string> &v, string word); // Now functions can insert any string as testcode.
-int listInsert(list <string> &l, string word);
-int setInsert(set <string> &s, string word);
-int vecDelete(vector <string> &v, string word);
-int listDelete(list <string> &l, string word);
-int setDelete(set <string> &s, string word);
+int vecRead(vector < string > & v);
+int listRead(list < string > & l);
+int setRead(set < string > & s);
+int vecSort(vector < string > & v);
+int listSort(list < string > & l);
+int vecInsert(vector < string > & v, string word); // Now functions can insert any string as test code.
+int listInsert(list < string > & l, string word);
+int setInsert(set < string > & s, string word);
+int vecDelete(vector < string > & v); // Functions will delete middle element, no need to pass string.
+int listDelete(list < string > & l);
+int setDelete(set < string > & s);
 
 int main() {
     vector < string > v;
@@ -30,38 +29,20 @@ int main() {
     int vecReadDuration = vecRead(v);
     int listReadDuration = listRead(l);
     int setReadDuration = setRead(s);
-// Begin sorting race.
+    // Begin sorting race.
     int vecSortDuration = vecSort(v);
     int listSortDuration = listSort(l);
     int setSortDuration = -1; // Set is already sorted, set as -1.
-//Begin race for inserting "TESTCODE" in middle of each container, or into set.
+    //Begin race for inserting "TESTCODE" in middle of each container, or into set.
 
     int vecInsertDuration = vecInsert(v, "TESTCODE");
     int listInsertDuration = listInsert(l, "TESTCODE");
     int setInsertDuration = setInsert(s, "TESTCODE");
-// Begin race for deleting middle element from each container.
-    int vecDeleteDuration;
-    int listDeleteDuration;
-    int setDeleteDuration;
-    // Delete middle element from vector using erase method.
-    auto deleteStart = high_resolution_clock::now();
-    v.erase(v.begin() + v.size() / 2); // Same method as insert but deleting instead. Still pretty fast.
-    auto deleteEnd = high_resolution_clock::now();
-    vecDeleteDuration = duration_cast < nanoseconds > (deleteEnd - deleteStart).count();
-    // Delete middle element from list using erase method. Need to use an iterator to find the middle of the list.
-    deleteStart = high_resolution_clock::now();
-    it_list = l.begin();
-    advance(it_list, l.size() / 2); // Move the iterator to the middle of list to delete middle element.
-    l.erase(it_list);
-    deleteEnd = high_resolution_clock::now();
-    listDeleteDuration = duration_cast < nanoseconds > (deleteEnd - deleteStart).count();
-    // Delete middle element from set using erase method.
-    deleteStart = high_resolution_clock::now();
-    auto it_set = s.begin();
-    advance(it_set, s.size() / 2); // Move the iterator to the middle of set to delete middle element.
-    s.erase(it_set);
-    deleteEnd = high_resolution_clock::now();
-    setDeleteDuration = duration_cast < nanoseconds > (deleteEnd - deleteStart).count();
+    // Begin race for deleting middle element from each container.
+    int vecDeleteDuration = vecDelete(v);
+    int listDeleteDuration = listDelete(l);
+    int setDeleteDuration = setDelete(s);
+
     // Output results in a table format using iomanip for formatting.
     cout << left << setw(20) << "Operation" << setw(20) << "Vector (ns)" << setw(20) << "List (ns)" << setw(20) << "Set (ns)" << endl;
     cout << left << setw(20) << "Read" << setw(20) << vecReadDuration << setw(20) << listReadDuration << setw(20) << setReadDuration << endl;
@@ -72,9 +53,9 @@ int main() {
     return 0;
 }
 // This will also cut out need for resetting EOF flag and resetting file pointer to beginning of file.
-int vecRead(vector <string> &v) {
+int vecRead(vector < string > & v) {
     // Function to read words into vector and time the operation. Will be used in main for scalability.
-   ifstream inFile("codes.txt");
+    ifstream inFile("codes.txt");
     if (!inFile) { // check if file opened successfully
         cerr << "Unable to open file codes.txt" << endl;
         return 1;
@@ -91,9 +72,9 @@ int vecRead(vector <string> &v) {
     return time; // Returns time taken to read into vector.
 }
 
-int listRead(list <string> &l) {
+int listRead(list < string > & l) {
     // Function to read words into list and time the operation. Will be used in main for scalability.
-   ifstream inFile("codes.txt");
+    ifstream inFile("codes.txt");
     if (!inFile) { // check if file opened successfully
         cerr << "Unable to open file codes.txt" << endl;
         return 1;
@@ -110,9 +91,9 @@ int listRead(list <string> &l) {
     return time;
 }
 
-int setRead(set <string> &s) {
+int setRead(set < string > & s) {
     // Function to read words into set and time the operation. Will be used in main for scalability.
-   ifstream inFile("codes.txt");
+    ifstream inFile("codes.txt");
     if (!inFile) { // check if file opened successfully
         cerr << "Unable to open file codes.txt" << endl;
         return 1;
@@ -129,7 +110,7 @@ int setRead(set <string> &s) {
     return time;
 }
 
-int vecSort(vector <string> &v) {
+int vecSort(vector < string > & v) {
     auto start = high_resolution_clock::now(); // Starts the timer for sorting vector
     sort(v.begin(), v.end()); // Uses sort function from algorithm library.
     auto end = high_resolution_clock::now();
@@ -137,7 +118,7 @@ int vecSort(vector <string> &v) {
     return time;
 }
 
-int listSort(list <string> &l) {
+int listSort(list < string > & l) {
     auto start = high_resolution_clock::now(); // Starts the timer for sorting list
     l.sort();
     auto end = high_resolution_clock::now();
@@ -145,7 +126,7 @@ int listSort(list <string> &l) {
     return time;
 }
 
-int vecInsert(vector <string> &v, string word) {
+int vecInsert(vector < string > & v, string word) {
     auto start = high_resolution_clock::now();
     v.insert(v.begin() + v.size() / 2, word); // Middle is found by dividing vector.size by 2. Pretty fast.
     auto end = high_resolution_clock::now();
@@ -153,9 +134,9 @@ int vecInsert(vector <string> &v, string word) {
     return time;
 }
 
-int listInsert(list <string> &l, string word) {
+int listInsert(list < string > & l, string word) {
     auto start = high_resolution_clock::now();
-    auto it_list = l.begin();
+    auto it_list = l.begin(); // Iterator created that starts at beginning.
     advance(it_list, l.size() / 2); // Move the iterator to the middle of the list. Slower since it moves from beginning to middle.
     l.insert(it_list, word);
     auto end = high_resolution_clock::now();
@@ -163,9 +144,35 @@ int listInsert(list <string> &l, string word) {
     return time;
 }
 
-int setInsert(set <string> &s, string word) {
+int setInsert(set < string > & s, string word) {
     auto start = high_resolution_clock::now();
     s.insert(word);
+    auto end = high_resolution_clock::now();
+    int time = duration_cast < nanoseconds > (end - start).count();
+    return time;
+}
+
+int vecDelete(vector < string > & v) {
+    auto start = high_resolution_clock::now();
+    v.erase(v.begin() + v.size() / 2); // Same method as insert but deleting instead. Still pretty fast.
+    auto end = high_resolution_clock::now();
+    int time = duration_cast < nanoseconds > (end - start).count();
+    return time;
+}
+
+int listDelete(list < string > & l) {
+    auto start = high_resolution_clock::now();
+    auto it_list = l.begin(); // Iterator created that starts at beginning.
+    advance(it_list, l.size() / 2); // Move the iterator to the middle of the list.
+    l.erase(it_list); // Takes iterator as argument to delete middle element.
+    auto end = high_resolution_clock::now();
+    int time = duration_cast < nanoseconds > (end - start).count();
+    return time;
+}
+
+int setDelete(set < string > & s) {
+    auto start = high_resolution_clock::now();
+    s.erase( * s.begin()); // Erase the first element in the set.
     auto end = high_resolution_clock::now();
     int time = duration_cast < nanoseconds > (end - start).count();
     return time;
